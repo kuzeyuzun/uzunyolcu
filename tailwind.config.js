09b84c120ba8b7d2eec57311940fe2d65e509932
Kuzey Uzun <kuzey.uzun@pttem.local>
2025-06-26 22:05:39 +0300

    yeni temea2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,21 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class', // veya 'media'
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     extend: {
       colors: {
-        background: '#18181b', // ana arka plan
-        surface: '#23232a',    // kartlar/alanlar
-        primary: '#38bdf8',    // sky-400
-        secondary: '#a3e635',  // lime-400
-        text: '#e5e7eb',       // nötr beyazımsı metin
-        accent: '#f472b6',     // pembe vurgular
+        background: '#18181b',
+        surface: '#23232a',
+        primary: '#38bdf8',
+        secondary: '#a3e635',
+        text: '#e5e7eb',
+        accent: '#f472b6',
       },
-      borderRadius: {
-        'xl': '1.25rem',
-      }
     },
   },
   plugins: [],
